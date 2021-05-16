19a60034600627712602ec283bd25a2a3191feac
yuni-the-advance <yuni.the.advance@gmail.com>
2021-05-17 03:56:38 +0900

    some change for nvel

--- javascript/novle-OneShort-loader.js
+++ javascript/novle-OneShort-loader.js
@@ -1,12 +1,16 @@
 $(function() {
-    json = "/json/novel/NovelAndArticle.json";
+    json = "/json/novel/novel.json";
     target = $('.OneShort');
-    $.getJSON(json, function(data1, data2) {
-        data2 = data1.novel.OneShort;
-        for (var n in data2) {
-            var text = '<p>';
-            text += "";
-            $(target).append(text);
+    $.getJSON(json, function(data) {
+        for (var n in data) {
+            var text = '<div class=\"Card\">';
+            text += "<h3><a href=\"" + data[n].url + "\">" + data[n].title + "</a></h3>";
+            text += "<p class=\"caption\">" + data[n].caption + "</p>";
+            text += "<p class=\"date\">" + data[n].date.year + "/" + data[n].date.month + "/" + data[n].date.day + "</p>";
+            text += '</div>';
+            if (data[n].type == "OneShort") {
+                $(target).append(text);
+            }
         }
     });
 });
