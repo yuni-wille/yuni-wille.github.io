9c6f272d52aebd948d53a3ca7a45411c15600e0d
yuni-the-advance <yuni.the.advance@gmail.com>
2021-06-06 16:26:52 +0900

    add report and add log and change some file

--- javascript/report-loader.js
+++ javascript/report-loader.js
@@ -1,12 +1,12 @@
 $(function() {
     json = "/json/report/report.json";
-    target = $('.may');
+    target = $('.first');
     $.getJSON(json, function(data) {
         for (var n in data) {
             var text = '<div class=\"Card\">';
-            if (data[n].month == "may") {
+            if (data[n].month == "first") {
                 text += "<h3><a href=\"" + data[n].url + "\">" + data[n].title + "</a>" + "　　" + data[n].date + "</h3>";
-                target = $('.may');
+                target = $('.first');
             } else {
                 target = $('.old');
             }
