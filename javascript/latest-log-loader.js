1709022b72152dbc8b3ce167fd20ceddb7ca1ff2
yuni-the-advance <yuni.the.advance@gmail.com>
2021-06-07 15:02:44 +0900

    add log

--- javascript/latest-log-loader.js
+++ javascript/latest-log-loader.js
@@ -1,12 +1,14 @@
 $(function() {
     json = "/json/log/log.json";
     target = $('.log-area');
-    $.getJSON(json, function(data, status) {
+    $.getJSON(json, function(data) {
         for (var n in data) {
             var text = '<p>';
             text += data[n].date.year + '/' + data[n].date.month + '/' + data[n].date.day;
             text += '<span>' + data[n].date.time + '</span>' + '<span>' + data[n].text + '</span>';
             text += '</p>'
+            if (data[n].date.month != 6)
+                text = null;
             $(target).append(text);
         }
     });
